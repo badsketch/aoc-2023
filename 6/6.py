3d993ddbea33e4db226ba358aaa791632e6a43df
badsketch <danielwang405@gmail.com>
2023-12-06 23:54:02 -0500

    Day 6 p2

--- 6/6.py
+++ 6/6.py
@@ -1,12 +1,13 @@
 from functools import reduce
 from typing import List
 from collections import namedtuple
+from concurrent.futures import ThreadPoolExecutor
 
 
 Race = namedtuple("Race", ["time", "distance"])
 
 def construct_races() -> List[Race]:
-    with open("6_input.txt") as f:
+    with open("6_input2.txt") as f:
         for line in f:
             if line.startswith("Time"):
                 times = line.split(":")[1].strip().split()
@@ -32,10 +33,37 @@ def calc_margin_of_error(races: List[Race]) -> int:
     return reduce(lambda x, y: x * y, errors)
 
 
+def calc_total_wins(race: Race) -> int:
+    wins = 0
+    futures = []
+    with ThreadPoolExecutor() as ex:
+        BATCH_SIZE = 1000000
+        for i in range(0, race.time + 1, BATCH_SIZE):
+            futures.append(ex.submit(calc_win, i + 1, min(race.time, i + BATCH_SIZE), race.time, race.distance))
+    
+    for future in futures:
+        wins += future.result()
+    return wins
+
+
+def calc_win(start: int, end: int, time: int, dist: int) -> int:
+    wins = 0
+    for milli in range(start, end + 1):
+        if milli * (time - milli) > dist:
+            wins += 1
+    return wins
+
+
 def part1() -> int:
     races = construct_races()            
     return calc_margin_of_error(races)
 
 
+def part2() -> int:
+    race = construct_races()[0]
+    return calc_total_wins(race)
+
+
 if __name__ == "__main__":
-    print(part1())
+    # print(part1())
+    print(part2())
